19ae73cffdeff148157515a408e9cd6752cd362e
aylenfv <fissoreaylen@gmail.com>
2025-02-19 01:13:38 -0300

    Función 'agregarAmigos' terminada'

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+let inputAmigo = document.querySelector('input');
+
+function agregarAmigo() {
+    nuevoAmigo = inputAmigo.value;
+    console.log(nuevoAmigo);
+    if ( nuevoAmigo == "" ) {
+        alert("Por favor, inserte un nombre.");
+    } else {
+        amigos.push(nuevoAmigo);
+        inputAmigo.value = '';
+    }
+    console.log(amigos);
+}
